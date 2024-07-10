caa67af0cbe058ae30ea0704be81508bced56308
DevoAhmedIsmail <devo.ahmedismail@gmail.com>
2024-07-10 12:34:26 +0300

    add translation for sliders

--- src/Data/HomeSlider.ts
+++ src/Data/HomeSlider.ts
@@ -1,23 +1,24 @@
+import { t } from "i18next";
 import sliderImage1 from "../assets/images/products/slider-1.webp"; 
 import sliderImage2 from "../assets/images/products/slider-2.webp"; 
 
 const HomeSliderData = [
     {
-        label: "New Products",
-        title: "Flexible Chair",
-        description: "Praesentium voluptatum sit. Iure consequatur nesciunt unde ut repellat architecto id nobis repudiandae.",
+        label: "new-products",
+        title: "flexible-chair",
+        description: "product-desc",
         img: sliderImage1
     },
     {
-        label: "New Products",
-        title: "Flexible Chair",
-        description: "Praesentium voluptatum sit. Iure consequatur nesciunt unde ut repellat architecto id nobis repudiandae.",
+        label: "new-products",
+        title: "flexible-chair",
+        description: "product-desc",
         img: sliderImage2
     },
     {
-        label: "New Products",
-        title: "Flexible Chair",
-        description: "Praesentium voluptatum sit. Iure consequatur nesciunt unde ut repellat architecto id nobis repudiandae.",
+        label: "new-products",
+        title: "flexible-chair",
+        description: "product-desc",
         img: sliderImage2
     },
 ]
--- src/components/Home/HeroSection/HeroSlide.tsx
+++ src/components/Home/HeroSection/HeroSlide.tsx
@@ -1,3 +1,4 @@
+import { t } from "i18next";
 import { Button } from "../../ui/button";
 
 type THeroSliderProps = {
@@ -14,15 +15,15 @@ const HeroSlide = ({title, description, img, label}: THeroSliderProps) => {
             <div className="flex flex-col items-center justify-center gap-5 text-center md:text-start md:flex-row ">
                 <div className="flex-1">
                     <div className="flex flex-col gap-2 mb-12">
-                        <p className="p-medium-24 ">{label}</p>
-                        <h1 className="h1-bold">{title}</h1>
-                        <p className="p-regular-16">{description}</p>
+                        <p className="p-medium-24 ">{t(label)}</p>
+                        <h1 className="h1-bold">{t(title)}</h1>
+                        <p className="p-regular-16">{t(description)}</p>
                     </div>
-                    <Button className="rounded-[3px] bg-mainColor hidden md:block" size="lg">Shop Now</Button>
+                    <Button className="rounded-[3px] bg-mainColor hidden md:block" size="lg">{t("shop-now")}</Button>
                 </div>
                 <div className="flex-1">
                     <img src={img} alt="title" className="object-contain w-full h-96" />
-                    <Button className="rounded-[3px] bg-mainColor block md:hidden mx-auto" size="lg">Shop Now</Button>
+                    <Button className="rounded-[3px] bg-mainColor block md:hidden mx-auto" size="lg">{t("shop-now")}</Button>
                 </div>
             </div>
 
