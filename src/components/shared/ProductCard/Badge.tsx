000eabce07e75ab5be7432411a767d24919d937e
DevoAhmedIsmail <devo.ahmedismail@gmail.com>
2024-07-12 01:21:32 +0300

    fix badge size

--- src/components/shared/ProductCard/Badge.tsx
+++ src/components/shared/ProductCard/Badge.tsx
@@ -7,14 +7,14 @@ type IBadgeProps = {
 
 const Badge = ({ discount, isNew }: IBadgeProps) => {
   return (
-    <div className="absolute flex flex-col gap-2 left-4 top-4">
+    <div className="absolute flex flex-col items-center gap-2 left-4 top-4">
       {discount && (
-        <span className="px-2 py-1 text-sm text-white rounded-md bg-mainColor">
+        <span className="w-[46px] h-[26px] flex-center p-medium-14 text-white rounded-md bg-mainColor">
           -{discount}%
         </span>
       )}
       {isNew && (
-        <span className="px-2 py-1 text-sm text-white bg-green-700 rounded-md">
+        <span className="w-[46px] h-[26px] flex-center p-medium-14 text-white bg-green-700 rounded-md">
           {t("new")}
         </span>
       )}
